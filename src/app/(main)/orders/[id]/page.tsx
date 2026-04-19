07c07c24eb5c13223d146309a6568754eb2a2d86
Cel44 <celineoei04@gmail.com>
2026-04-19 22:54:39 +0800

    feat(order): added order[id] page

---
+++ src/app/(main)/orders/[id]/page.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { useOrderDetail } from '@/lib/hooks/useOrders'
+
+export default function OrderDetailPage() {
+  const { id } = useParams<{ id: string }>()
+
+  const { data: order, isLoading } = useOrderDetail(id)
+
+  function formatDate(dateString: string) {
+    return new Date(dateString).toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    })
+  }
+
+  if (isLoading) return <div>Loading...</div>
+  if (!order) return <div>Order not found</div>
+
+  return (
+    <div className="max-w-4xl mx-auto p-6 space-y-6">
+      
+      {/* Header */}
+      <div>
+        <h1 className="text-2xl font-semibold">
+          Order #{order.id}
+        </h1>
+        <p className="text-gray-600">
+          Status: {order.status}
+        </p>
+      </div>
+
+      {/* Items */}
+      <div className="space-y-4">
+        {order.items.map((item) => (
+          <div
+            key={item.productId}
+            className="border rounded-lg p-4 flex gap-4"
+          >
+
+            {/* Info */}
+            <div className="flex-1 space-y-1">
+              <h2 className="font-medium">{item.productName}</h2>
+              <p className="text-sm text-gray-500">
+                Size: {item.size}
+              </p>
+              <p className="text-sm text-gray-500">
+                Quantity: {item.quantity}
+              </p>
+
+               <p className="text-sm text-gray-500">
+                   Rental Duration: {item.rentalDays} days
+               </p>
+
+              <p className="text-sm text-gray-500">
+                  Rental Period: {formatDate(item.startDate)} to {formatDate(item.endDate)}
+               </p>
+            </div>
+
+            {/* Price */}
+            <div className="text-right font-medium">
+               Rp. {item.subtotal.toLocaleString('id-ID')}
+            </div>
+
+          </div>
+
+            
+        ))}
+      </div>
+
+      {/* Total */}
+      <div className="border-t pt-4 text-right">
+         <p className='mb-2 text-lg font-semibold'> 
+            Shipping: Rp. {order.shippingCost.toLocaleString('id-ID')} 
+         </p>
+
+         <p className="text-lg font-semibold">
+            Total: Rp. {order.total.toLocaleString('id-ID')}
+         </p>
+      </div>
+
+    </div>
+  )
+}
